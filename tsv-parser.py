8a8e0ecc9e96f3b0de39c1fe0b1eb6003379629e
George Mack <dreambeamz@gmail.com>
2015-05-16 09:05:39 -0400

    Bug fixes and better output

--- tsv-parser.py
+++ tsv-parser.py
@@ -38,22 +38,22 @@ headers = file_lines[0]
 file_lines = file_lines[1:]
 
 headers2 = file_lines2[0]
-file_lines2 = [x for x in file_lines2[1:] if len(x) > 6 and x[6] in keepers]
+file_lines2 = [x for x in file_lines2[1:]
+    if len(x) > 6 and x[6] in keepers and
+    x[7].strip() == "Eventbrite Completed"]
 
 buyer_emails = [x[4].strip().lower() for x in file_lines]
 confirm_emails = [x[5].strip().lower() for x in file_lines]
 
-email_dict2 = {x[4]:x for x in file_lines2}
+email_dict2 = {x[4].strip().lower():x for x in file_lines2}
 
-count = 0
-dup_user_count = 0
-
-you_suck = set()
+incorrect_emails = []
+dup_users = set()
 
 def get_counts(email):
-    if email in you_suck:
+    if email in dup_users:
         return
-    global count, line_count, dup_user_count
+    global count, line_count
     be_count = buyer_emails.count(email)
     ce_count = confirm_emails.count(email)
     if be_count > 1 or ce_count > 1:
@@ -69,15 +69,13 @@ def get_counts(email):
             ]
         be_lines.sort()
         ce_lines.sort()
-        you_suck.add(email)
+        dup_users.add(email)
         if be_lines != ce_lines:
-            print(email, be_lines, ce_lines)
-            dup_user_count += 1
+            incorrect_emails.append((email, be_lines, ce_lines))
         else:
             file_lines[be_lines[0]][9] = "Duplicates on lines {0} {1} ".format(
                 [x + 2 for x in be_lines], [x + 2 for x in ce_lines]
                 ) + file_lines[be_lines[0]][9]
-        count += 1
 
 for be, ce in zip(buyer_emails, confirm_emails):
     get_counts(be)
@@ -85,28 +83,61 @@ for be, ce in zip(buyer_emails, confirm_emails):
         get_counts(ce)
 
 TO_BE_REFUNDED = []
+NOT_REFUNDING_COUNT = 0
+NOT_PURCHASED_FROM_EB_COUNT = 0
+WAS_DUPLICATE_COUNT = 0
+NOT_IN_SPREADSHEET2_COUNT = 0
 
 for x in file_lines:
     emails = set(x[4:6])
-    if x[7].strip().lower() == "50% refund" and \
-    "eventbrite" in x[6].lower() and \
-    not [y for y in emails if y not in you_suck] and \
-    [y for y in emails if y in email_dict2]:
-        email = [y for y in emails if y in email_dict2][0]
-        z = email_dict2[email]
-        value = (z[0], str(round(float(z[8]) * 0.5, 2)))
-        TO_BE_REFUNDED.append(value)
+    if x[7].strip().lower() == "50% refund":
+        was_purchased_from_eb = "eventbrite" in x[6].lower()
+        was_not_dup = not not [y for y in emails if y not in dup_users]
+        was_in_spreadsheet2 = not not [y for y in emails if y in email_dict2]
+        all_reqs = [was_purchased_from_eb, was_not_dup, was_in_spreadsheet2]
+        if all(all_reqs):
+            email = [y for y in emails if y in email_dict2][0]
+            z = email_dict2[email]
+            value = (z[0], str(round(float(z[8]) * 0.5, 2)))
+            TO_BE_REFUNDED.append(value)
+        else:
+            NOT_REFUNDING_COUNT += 1
+            if not was_purchased_from_eb:
+                NOT_PURCHASED_FROM_EB_COUNT += 1
+            if not was_not_dup:
+                WAS_DUPLICATE_COUNT += 1
+            if not was_in_spreadsheet2:
+                NOT_IN_SPREADSHEET2_COUNT += 1
 
-print(count, "duplicate lines")
-print(dup_user_count, "people typed in their email address wrong")
-print(len(TO_BE_REFUNDED), "people are getting a refund")
-print(round(sum(float(x[1]) for x in TO_BE_REFUNDED), 2), "dollars refunded")
+
+print("\nRefund stats:")
+print("{0} Requested refund but not eligible "
+    "for a refund".format(NOT_REFUNDING_COUNT))
+print("{0} Requested refund but did not purchase from "
+    "Eventbrite".format(NOT_PURCHASED_FROM_EB_COUNT))
+print("{0} Requested refund, but submitted multiple "
+    "requests".format(WAS_DUPLICATE_COUNT))
+print("{0} Requested refund, but was not in "
+    "spreadsheet 2 (or order type was not "
+    "'Eventbrite Completed')".format(NOT_IN_SPREADSHEET2_COUNT))
+print("{0} people are getting a refund".format(len(TO_BE_REFUNDED)))
+print("${0} refunded".format(
+    round(sum(float(x[1]) for x in TO_BE_REFUNDED), 2)))
+print("\nOther stats:")
+print("{0} people submitted multiple requests for "
+    "a refund".format(len(dup_users)))
+print("{0} people typed in their email address "
+    "incorrectly".format(len(incorrect_emails)))
+
 
 # By cancellation credit option
 canc_options = [x[7] for x in file_lines]
 option_list = set(canc_options)
 for option in sorted(option_list):
-    print(option, canc_options.count(option))
+    print("Cancellation option '{0}': {1}".format(
+        option, canc_options.count(option)))
+
+print("\n")
 
 file_lines.insert(0, headers)
 file_lines = ["\t".join(x) for x in file_lines]
