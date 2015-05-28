a02c74c4967f1b60fdeaa2e3af4719ae3d0c355c
George Mack <dreambeamz@gmail.com>
2015-05-27 22:21:11 -0400

    Add a new report for users requesting tickets

--- tsv-parser.py
+++ tsv-parser.py
@@ -6,11 +6,15 @@ Created on Tue May 12 19:37:11 2015
 """
 
 Notes = """
-Spreadsheet1:  ColumnH  50% Refund -> Column{E,F} (email address) ->  (also grab row#)
-Spreadsheet2: if Column H (OrderType) == "Eventbrite Completed": Column A (OrderNumber) -> Column I (Total Paid)
+We are going by whatever their last request was if they made multiple request
 
-Output:
-Order Number, Total Paid * 0.5      (<-and add to new columns in spreadsheet 1)
+List of people (by order number) to get refunds and how much.
+Call it $amount auto sent 2 on column I. This includes people that had
+spaces in their e-mail address before or after their e-mail address that
+werent previously refunded
+
+List of people to get something wickeds and how many along with their
+order number
 """
 
 import sys
@@ -83,48 +87,69 @@ for be, ce in zip(buyer_emails, confirm_emails):
 
 TO_BE_REFUNDED = []
 NOT_REFUNDING_COUNT = 0
-NOT_PURCHASED_FROM_EB_COUNT = 0
-WAS_DUPLICATE_COUNT = 0
-NOT_IN_SPREADSHEET2_COUNT = 0
+R_NOT_PURCHASED_FROM_EB_COUNT = 0
+R_WAS_DUPLICATE_COUNT = 0
+R_NOT_IN_SPREADSHEET2_COUNT = 0
+
+NOT_TICKETING_COUNT = 0
+TICKETS_TO_SOWI = []
+
+T_NOT_PURCHASED_FROM_EB_COUNT = 0
+T_WAS_DUPLICATE_COUNT = 0
+T_NOT_IN_SPREADSHEET2_COUNT = 0
+
 
 for x in file_lines:
     emails = set(a.strip().lower() for a in x[4:6])
-    if x[7].strip().lower() == "50% refund":
-        was_purchased_from_eb = "eventbrite" in x[6].lower()
-        was_not_dup = not not [y for y in emails if y not in dup_users]
-        was_in_spreadsheet2 = not not [y for y in emails if y in email_dict2]
-        all_reqs = [was_purchased_from_eb, was_not_dup, was_in_spreadsheet2]
-        if all(all_reqs):
-            email = [y for y in emails if y in email_dict2][0]
-            z = email_dict2[email]
+    was_purchased_from_eb = "eventbrite" in x[6].lower()
+    was_not_dup = not not [y for y in emails if y not in dup_users]
+    was_in_spreadsheet2 = not not [y for y in emails if y in email_dict2]
+    all_reqs = [was_purchased_from_eb, was_not_dup, was_in_spreadsheet2]
+    user_option = x[7].strip().lower()
+    if all(all_reqs):
+        email = [y for y in emails if y in email_dict2][0]
+        z = email_dict2[email]
+        if user_option == "50% refund":
             value = (z[0], str(round(float(z[8]) * 0.5, 2)))
             TO_BE_REFUNDED.append(value)
-            dollar_val = "${0} auto sent".format(value[1])
+            dollar_val = "${0} auto sent 2".format(value[1])
             if x[8].strip():
                 x[8] = " // ".join([dollar_val, x[8]])
             else:
                 x[8] = dollar_val
-        else:
+        elif user_option == 'tickets to something wicked 2015':
+            value = (z[0], int(z[5]))
+            TICKETS_TO_SOWI.append(value)
+    else:
+        if user_option == "50% refund":
             NOT_REFUNDING_COUNT += 1
             if not was_purchased_from_eb:
-                NOT_PURCHASED_FROM_EB_COUNT += 1
+                R_NOT_PURCHASED_FROM_EB_COUNT += 1
             if not was_not_dup:
-                WAS_DUPLICATE_COUNT += 1
+                R_WAS_DUPLICATE_COUNT += 1
             if not was_in_spreadsheet2:
-                NOT_IN_SPREADSHEET2_COUNT += 1
+                R_NOT_IN_SPREADSHEET2_COUNT += 1
+        elif user_option == 'tickets to something wicked 2015':
+            NOT_TICKETING_COUNT += 1
+            if not was_purchased_from_eb:
+                T_NOT_PURCHASED_FROM_EB_COUNT += 1
+            if not was_not_dup:
+                T_WAS_DUPLICATE_COUNT += 1
+            if not was_in_spreadsheet2:
+                T_NOT_IN_SPREADSHEET2_COUNT += 1
 
 
 print("\nRefund stats:")
 print("* {0} Requested refund but not eligible "
     "for a refund".format(NOT_REFUNDING_COUNT))
 print("* {0} Requested refund but did not purchase from "
-    "Eventbrite".format(NOT_PURCHASED_FROM_EB_COUNT))
+    "Eventbrite".format(R_NOT_PURCHASED_FROM_EB_COUNT))
 print("* {0} Requested refund, but submitted multiple "
-    "requests".format(WAS_DUPLICATE_COUNT))
+    "requests".format(R_WAS_DUPLICATE_COUNT))
 print("* {0} Requested refund, but was not in "
     "spreadsheet 2 (or order type was not "
     "'Eventbrite Completed' or ticket type not in "
-    "{1})".format(NOT_IN_SPREADSHEET2_COUNT, keepers))
+    "{1})".format(R_NOT_IN_SPREADSHEET2_COUNT, keepers))
 print("* {0} people are getting a refund".format(len(TO_BE_REFUNDED)))
 print("* ${0} refunded".format(
     round(sum(float(x[1]) for x in TO_BE_REFUNDED), 2)))
@@ -133,8 +158,20 @@ print("* {0} people submitted multiple requests for "
     "a refund".format(len(dup_users)))
 print("* {0} people typed in their email address "
     "incorrectly".format(len(incorrect_emails)))
-
-
+print("*" * 79)
+print("* {0} Requested tickets but did not purchase from "
+    "Eventbrite".format(T_NOT_PURCHASED_FROM_EB_COUNT))
+print("* {0} Requested tickets, but submitted multiple "
+    "requests".format(T_WAS_DUPLICATE_COUNT))
+print("* {0} Requested tickets, but was not in "
+    "spreadsheet 2 (or order type was not "
+    "'Eventbrite Completed' or ticket type not in "
+    "{1})".format(T_NOT_IN_SPREADSHEET2_COUNT, keepers))
+print("* {0} people requested tickets to SOWI but were not eligible".format(
+    NOT_TICKETING_COUNT))
+print("* {0} people are receiving {1} tickets to Something Wicked".format(
+    len(TICKETS_TO_SOWI), sum(int(x[1]) for x in TICKETS_TO_SOWI)))
+print("*" * 79)
 # By cancellation credit option
 canc_options = [x[7] for x in file_lines]
 option_list = set(canc_options)
@@ -156,3 +193,8 @@ TO_BE_REFUNDED = ["\t".join(x) for x in TO_BE_REFUNDED]
 with open("to-be-refunded.tsv", "w") as fh:
     print("Creating {0}".format(fh.name))
     fh.write("\n".join(TO_BE_REFUNDED))
+
+TICKETS_TO_SOWI = ["\t".join(str(y) for y in x) for x in TICKETS_TO_SOWI]
+
+with open("tickets-to-sowi.tsv", "w") as fh:
+    fh.write("\n".join(TICKETS_TO_SOWI))
